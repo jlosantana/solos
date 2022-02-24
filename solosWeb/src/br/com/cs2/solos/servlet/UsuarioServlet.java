359e3602a9042fee05b620a68a42eaaeb0b7ba5a
Joao Santana <joao.santana@sensedia.com>
2022-02-24 09:51:03 -0300

    integração das camadas controller (servlet) com a camada de negócio (model/business) utilizando o lookup para criar um EJB

--- solosWeb/src/br/com/cs2/solos/servlet/UsuarioServlet.java
+++ solosWeb/src/br/com/cs2/solos/servlet/UsuarioServlet.java
@@ -1,23 +1,58 @@
 package br.com.cs2.solos.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.cs2.solos.business.UsuarioBusinessLocal;
+import br.com.cs2.solos.model.UsuarioModel;
+
 public class UsuarioServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	
+	private InitialContext context;
+	
+	private UsuarioBusinessLocal usuarioBusiness;
 
+	// Construtor
 	public UsuarioServlet() {
 		super();
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		
+		// Executo o código da classe mão para evitar problemas
+		super.init();
+
+		// Meu código
+		try {
+			// executa a criação do contexto
+			context = new InitialContext();
+			
+			usuarioBusiness = (UsuarioBusinessLocal) context.lookup("solosEAR/UsuarioBusiness/local");
+			
+		} catch (Exception e) {
+			// se ocorrer erro caia aqui nesse bloco
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("executing get ...");
+		
+		List<UsuarioModel> listaUsuarios = usuarioBusiness.listar("João");
+		
+		for (UsuarioModel usuarioModel : listaUsuarios) {
+			System.out.println("Nome do usuário: " + usuarioModel.getNome());
+		}
 	}
 
 	@Override
