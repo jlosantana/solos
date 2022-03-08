9980e008193988fb0187d4e918e658b3fea265f3
Joao Santana <joao.santana@sensedia.com>
2022-03-08 11:23:10 -0300

    Inclusao da consulta na tabela de banco de dados

--- solosEJB/ejbModule/br/com/cs2/solos/business/UsuarioBusiness.java
+++ solosEJB/ejbModule/br/com/cs2/solos/business/UsuarioBusiness.java
@@ -3,6 +3,7 @@ package br.com.cs2.solos.business;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -14,55 +15,97 @@ import br.com.cs2.solos.model.UsuarioModel;
 
 @Stateless
 public class UsuarioBusiness implements UsuarioBusinessFacade {
-	
+
 	// Simulação do banco de dados utilizando o LinkedList
 	private List<UsuarioModel> usuarioDB = new LinkedList<>();
 
-    public UsuarioBusiness() {
-    }
+	public UsuarioBusiness() {
+	}
 
 	@Override
 	public UsuarioModel cadastrar(UsuarioModel novoUsuario) {
-		
+
 		System.out.println("cadastrando o usuário: " + novoUsuario.getNome() + ", " + novoUsuario.getCpf());
-		
+
 		try {
-		
+
 			Connection dbConecction = getConnection();
-			
+
 			String insertStatement = "INSERT INTO public.usuario(login, nome, cpf, senha) VALUES (?, ?, ?, ?)";
-			
+
 			PreparedStatement insertPreparedStatement = dbConecction.prepareStatement(insertStatement);
-			
+
 			insertPreparedStatement.setString(1, novoUsuario.getLogin());
 			insertPreparedStatement.setString(2, novoUsuario.getNome());
 			insertPreparedStatement.setString(3, novoUsuario.getCpf());
 			insertPreparedStatement.setString(4, novoUsuario.getSenha());
-			
+
 			insertPreparedStatement.execute();
-			
+
+			insertPreparedStatement.close();
 			dbConecction.close();
-		
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return novoUsuario;
 	}
 
 	@Override
 	public List<UsuarioModel> listar(String nomeDeUsuario) {
-		
+
 		System.out.println("buscando os usuários pelo nome: " + nomeDeUsuario);
-		
-		return usuarioDB;
+
+		try {
+
+			Connection dbConecction = getConnection();
+
+			String selectStatement = "select login, nome, cpf, senha from public.usuario";
+
+			PreparedStatement selectPreparedStatement = dbConecction.prepareStatement(selectStatement);
+
+			ResultSet resultado = selectPreparedStatement.executeQuery();
+
+			List<UsuarioModel> retorno = null;
+			UsuarioModel usuario = null;
+
+			while (resultado.next()) {
+
+				String login = resultado.getString(1);
+				String nome = resultado.getString(2);
+				String cpf = resultado.getString(3);
+				String senha = resultado.getString(4);
+
+				usuario = new UsuarioModel(nome);
+				usuario.setLogin(login);
+				usuario.setCpf(cpf);
+				usuario.setSenha(senha);
+
+				if (retorno == null) {
+					retorno = new LinkedList<>();
+				}
+
+				retorno.add(usuario);
+			}
+
+			resultado.close();
+			dbConecction.close();
+
+			return retorno;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
 	}
 
 	@Override
 	public UsuarioModel atualizar(UsuarioModel usuarioParaAtualizacao) {
-		
+
 		System.out.println("atualizando o usuario: " + usuarioParaAtualizacao.getNome());
-		
+
 		for (UsuarioModel usuarioCorrente : usuarioDB) {
 			if (usuarioParaAtualizacao.equals(usuarioCorrente)) {
 				// atualizando CPF
@@ -75,22 +118,22 @@ public class UsuarioBusiness implements UsuarioBusinessFacade {
 				usuarioCorrente.setSenha(usuarioParaAtualizacao.getSenha());
 			}
 		}
-		
+
 		return usuarioParaAtualizacao;
 	}
 
 	@Override
 	public void deletar(UsuarioModel usuarioParaDelecao) {
-		
+
 		System.out.println("deletando o usuário: " + usuarioParaDelecao.getNome());
-		
+
 		Iterator<UsuarioModel> it = usuarioDB.iterator();
-		
+
 		// enquanto for verdade
 		while (it.hasNext()) {
 			// faz um cast de Object para UsuarioModel
 			UsuarioModel usuarioCorrente = (UsuarioModel) it.next();
-			
+
 			// compara com o usuario recebido para exclusão
 			if (usuarioParaDelecao.equals(usuarioCorrente)) {
 				// pede para o iterator fazer a remoção do objeto da lista
@@ -98,15 +141,15 @@ public class UsuarioBusiness implements UsuarioBusinessFacade {
 				break;
 			}
 		}
-		
+
 	}
-	
+
 	public Connection getConnection() throws SQLException {
 
 		String url = "jdbc:postgresql://localhost:5432/solos?user=postgres&password=postgres";
 
 		Connection conn = DriverManager.getConnection(url);
-		
+
 		return conn;
 	}
 
--- solosWeb/WebContent/index.jsp
+++ solosWeb/WebContent/index.jsp
@@ -1,45 +1,57 @@
-<%@ page import="java.util.Date"%>
-<%@ page contentType="text/html; charset=UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<jsp:useBean id="indexBean" class="br.com.cs2.solos.bean.IndexBean"
-	scope="request" />
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="f" uri="http://java.sun.com/jsf/core"%>
+<%@ taglib prefix="h" uri="http://java.sun.com/jsf/html"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script
+	src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"
+	integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
+	crossorigin="anonymous"></script>
+
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Solos</title>
 </head>
 <body>
-	<%
-		Date dataAtual = new Date(); 
-	%>
-	<h1>
-		<jsp:getProperty name="indexBean" property="mensagemDeBoasVindas" /> - <%=dataAtual %>
-	</h1>
-	<%
-		String nomeUsuario = request.getParameter("nomeUsuario"); 
-	%>
-	
-	<c:if test="<%=nomeUsuario != null%>">
-		<h3>Nome do usuário enviado: <%=nomeUsuario %></h3>
-	</c:if>
-	
-	<form action="index.jsp" method="get">
-		<input name="nomeUsuario" />
-		<button type="submit">Enviar</button>
-	</form>
-	
-	<h1>Lista de Usuários</h1>
-	
-	<table border="1">
-		<tr>
-			<td>Nome</td>
-		</tr>
-		<c:forEach var="usuario" items="${indexBean.listaUsuarios}">
-		    <tr>
-		         <td>${usuario.nome}</td>
-		    </tr>
-		</c:forEach>
-	</table>
+	<div class="container">
+		<f:view>
+			<h:form>
+				<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed"
+							data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+							aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span> <span
+								class="icon-bar"></span> <span class="icon-bar"></span> <span
+								class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">SOLOS</a>
+					</div>
+			
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse"
+						id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li>						
+								<h:commandLink value="Lista de Usuários" action="listaUsuarios" />						
+							</li>
+							<li class="active"><a href="#">Novo Usuário <span class="sr-only">(current)</span></a></li>
+						</ul>
+					</div>
+					<!-- /.navbar-collapse -->
+				</div>
+				<!-- /.container-fluid --> 
+				</nav>
+			</h:form>
+			
+		</f:view>
+	</div>
 </body>
 </html>
